88e86251ee0be3980d68d985c8584b672c59c33a
19peirooo <victorpeiro05@gmail.com>
2025-12-09 22:13:45 +0100

    Login Page Metadata

---
+++ src/app/LoginPage.jsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { isAuthenticated, getSpotifyAuthUrl } from '@/lib/auth';
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Si ya está autenticado, redirigir al dashboard
+    if (isAuthenticated()) {
+      router.push('/dashboard');
+    }
+  }, [router]);
+
+  const handleLogin = () => {
+    window.location.href = getSpotifyAuthUrl();
+  };
+
+  return (
+      <>
+        <div className="flex justify-center items-center h-screen">
+          <div className='w-[60vw] h-[80vh] bg-radial from-black via-green-700 to-green-800 rounded-full flex flex-col justify-center items-center'>
+            <h1 className='text-5xl font-extrabold text-white drop-shadow-lg mb-8 text-center'>🎵 Spotify Taste Mixer</h1>
+            <button 
+              className="bg-gray-500 hover:bg-green-500 text-white font-semibold px-10 py-3 rounded-full shadow-lg transform transition duration-300 hover:scale-105" 
+              onClick={handleLogin}>
+                Login
+            </button>
+          </div>
+        </div>
+      </>  
+  );
+}
+
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,14 @@
-'use client';
+import LoginPage from "./LoginPage";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { isAuthenticated, getSpotifyAuthUrl } from '@/lib/auth';
-import { spotifyRequest } from '@/lib/spotify';
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Si ya está autenticado, redirigir al dashboard
-    if (isAuthenticated()) {
-      router.push('/dashboard');
-    }
-  }, [router]);
-
-  const handleLogin = () => {
-    window.location.href = getSpotifyAuthUrl();
-  };
-
-  return (
-      <>
-        <div className="flex justify-center items-center h-screen">
-          <div className='w-[60vw] h-[80vh] bg-radial from-black via-green-700 to-green-800 rounded-full flex flex-col justify-center items-center'>
-            <h1 className='text-5xl font-extrabold text-white drop-shadow-lg mb-8 text-center'>🎵 Spotify Taste Mixer</h1>
-            <button 
-              className="bg-gray-500 hover:bg-green-500 text-white font-semibold px-10 py-3 rounded-full shadow-lg transform transition duration-300 hover:scale-105" 
-              onClick={handleLogin}>
-                Login
-            </button>
-          </div>
-        </div>
-      </>  
-  );
+export const metadata = {
+    title: "Login",
+    description: "Pagina de Login"
 }
 
+export default function Home() {
 
+  return (
+    <LoginPage/>
+  )
+
+}
